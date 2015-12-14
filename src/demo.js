e2c07f44d51e43a4281fb5e34db76aa26145bae4
Trung Dinh Quang <trungdq88@gmail.com>
2015-12-14 15:34:08 +0700

    Add build tools. Add demo page

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,7 @@
  * Created by dinhquangtrung on 12/14/15.
  */
 import React from 'react';
+import './index.html';
 import ReactDOM from 'react-dom';
-
+import App from './app';
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+var buildDir = 'build/';
+
+var config = {
+  context: path.join(__dirname, 'src'),
+  cache: true,
+  debug: true,
+  target: 'web',
+  devtool: 'inline-source-map',
+  entry: {
+    demo: ['./demo.js']
+  },
+  output: {
+    path: buildDir,
+    publicPath: '/',
+    filename: 'index.js',
+    pathinfo: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: ['react-hot', 'babel-loader?optional[]=runtime&stage=0&plugins=rewire']
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=[path][name].[ext]'
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  devServer: {
+    contentBase: path.resolve(buildDir),
+    headers: { "Access-Control-Allow-Origin": "*" },
+    hot: true,
+    noInfo: false,
+    inline: true,
+    stats: { colors: true }
+  }
+};
+
+module.exports = config;
